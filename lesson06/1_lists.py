3b2e29a5d3f879921099819f38a1e462f1defe54
imvinayak32 <vinayaksrock@gmail.com>
2023-10-13 17:36:31 +0530

    added Theory part

--- lesson06/1_lists.py
+++ lesson06/1_lists.py
@@ -1,20 +1,26 @@
-users = ['Dave', 'John', 'Sara']
+# Different type of list:
 
-data = ['Dave', 42, True]
-
-emptylist = []
+users = ['Dave', 'John', 'Sara'] #same data
+data = ['Dave', 42, True] # different data can be here
+emptylist = [] #it could empty
+furniture = ['table', 'chair', 'rack', 'shelf']
 
 print("Dave" in emptylist)
 
+
+# Accessing the List
 print(users[0])
 print(users[-2])
 
 print(users.index('Sara'))
 
-print(users[0:2])
-print(users[1:])
-print(users[-3:-1])
+print(users[0:2]) # 2 is not included
+print(users[1:]) # from 1 till end
+print(users[-3:-1]) # -1 is not included
+print(furniture[:])
 
+
+# Length of list
 print(len(data))
 
 
--- lesson06/2_list_insert_delete.py
+++ lesson06/2_list_insert_delete.py
@@ -1,37 +1,46 @@
-users = ['vin', 'harshil', 'raj', 'hind', 'kar', 'gray']
+users = ['vin']
+data = ['Dave', 42, True]
+
+# List Methods :
 
 users.append('Elsa')
 print(users)
 
-users += ['Jason']
+users += ['Jason'] #added in the end
 print(users)
 
-users.extend(['Robert', 'Jimmy'])
-print(users)
+# users.extend(['Robert', 'Jimmy']) #added in the end
+# print(users)
 
-# users.extend(data)
+# users.extend(data) # two list can be mereged into one
 # print(users)
 
 users.insert(0, 'Bob')
 print(users)
 
-users[2:2] = ['Eddie', 'Alex']
+users.insert(2,'harsh')
 print(users)
 
-users[1:3] = ['Robert', 'JPJ']
+users.insert(1,'oggg')
 print(users)
 
-users.remove('Bob')
-print(users)
+# users[2:2] = ['Eddie', 'Alex']
+# print(users)
 
-print(users.pop())
-print(users)
+# users[1:3] = ['Robert', 'JPJ']
+# print(users)
 
-del users[0]
-print(users)
+# users.remove('Bob')
+# print(users)
 
-# del data
-data.clear()
-print(data)
+# print(users.pop())
+# print(users)
 
-users[1:2] = ['dave']
+# del users[0]
+# print(users)
+
+# # del data
+# data.clear()
+# print(data)
+
+# users[1:2] = ['dave']
